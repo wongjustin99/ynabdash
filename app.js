de89ad00355bc827f1e36b5dcf6af6893a98be77
Ben <benspaul@gmail.com>
2014-08-11 23:18:08 -0400

    Remove front-end controlled filters

--- app.js
+++ app.js
@@ -62,79 +62,10 @@ function TransactionController(settings){
   var self = this;
   self.transactions = ko.observableArray();
 
-  self.sortBy = ko.observable();
-  self.desc = ko.observable(true);
-
-  self.sort = function(column) {
-    return function() {
-      self.desc(!self.desc());
-      self.sortBy(column);
-    }
-  }
-
-  var filters = {
-    "date": new Filter("Date", "date", function(transaction) { 
-      return transaction.date;
-    }, function(transaction, on) {
-      return transaction.date === on.date;
-    }),
-    "category": new Filter("Category", "category", function(t) {
-      return t.categoryName;
-    }, function(t, on) {
-      return t.categoryId === on.categoryId;
-    })
-  }
-
-  var filtersObject = ko.observable({});
-
-  self.addFilter = function(name, transaction){
-    return function() {
-      var filtersObjectTemp = filtersObject();
-      var filter = filtersObjectTemp[name] = filters[name];
-      filter.on = transaction;
-      filtersObject(filtersObjectTemp);
-    }
-  }
-
-  self.removeFilter = function(name) {
-    return function() {
-      var filtersObjectTemp = filtersObject();
-      delete filtersObjectTemp[name];
-      filtersObject(filtersObjectTemp);
-    }
-  }
-
-  self.removeFilters = function(){
-    filtersObject({});
-  }
-
-  self.filters = ko.computed(function(){
-    return _.values(filtersObject());
-  })
-
   self.filteredTransactions = ko.computed(function(){
-    var sort = self.sortBy();
-    var desc = self.desc();
-    var filters = self.filters();
     var transactions = _.chain(self.transactions()).map(function(transaction){
       return new Transaction(settings.app, transaction);
     });
-
-    filters.forEach(function(filter){
-      transactions = transactions.filter(function(transaction){
-        return filter.predicate(transaction, filter.on);
-      })
-    })
-
-    if(sort) {
-      transactions = transactions.sortBy(function(transaction){
-        return transaction[sort];
-      })
-
-      if(desc) {
-        transactions = transactions.reverse();
-      }
-    }
 
     return transactions.value();
   })
