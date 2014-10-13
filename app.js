370c7482cacae94850d6d47e9b73a68519a36a65
Ben <benspaul@gmail.com>
2014-10-12 22:13:08 -0400

    Use dates in current month rather than always 31

--- app.js
+++ app.js
@@ -102,9 +102,13 @@ function BudgetController(settings){
   var client = settings.client;
   var app = settings.app;
 
+  self.budgetMetaPath = 'YNAB/Budget~9674B08A.ynab4/Budget.ymeta';
+  self.relativeDataFolderName = 'data1~05BB9702';
+
   self.budgets = ko.observableArray();
   self.budget = ko.observable();
   self.budgetDataFolder = ko.observable()
+  self.devices = ko.observableArray();
   self.device = ko.observable();
   self.loadingProgress = ko.observable(0);
   self.loadingMessages = ko.observableArray();
@@ -112,31 +116,33 @@ function BudgetController(settings){
 
   self.budgetName = ko.computed(function(){
     return ((self.budget() || "").split("/")[1] || "").split("~")[0];
-  })
+  });
 
-  self.budgetMetaPath = ko.computed(function(){
-    return [self.budget(), budgetMetaFile].join("/")
-  })
   self.budgetDataPath = ko.computed(function(){
     return [self.budget(), self.budgetDataFolder()].join("/")
-  })
+  });
+
   self.budgetDevicesPath = ko.computed(function(){
     return [self.budgetDataPath(), "devices"].join("/")
-  })
+  });
+
   self.deviceFilePath = function(deviceFileName){
     return [self.budgetDevicesPath(), deviceFileName].join("/")
-  }
+  };
+
   self.fullBudgetPath = ko.computed(function(){
     if(self.device()){
       return [self.budgetDataPath(), self.device().deviceGUID].join("/");
     }
-  })
+  });
+
   self.fullBudgetSettings = ko.computed(function(){
     return [self.fullBudgetPath(), "budgetSettings.ybsettings"].join("/");
-  })
+  });
+
   self.fullBudgetFile = ko.computed(function(){
     return [self.fullBudgetPath(), "Budget.yfull"].join("/");
-  })
+  });
 
   self.loading = function(percent, message) {
     self.loadingProgress(percent);
@@ -148,10 +154,10 @@ function BudgetController(settings){
     self.device(null);
 
     self.loading(10, "Looking up where the YNAB data folder is ...");
-    client.loadJson(self.budgetMetaPath()).then(function(data){
+    client.loadJson(self.budgetMetaPath).then(function(data){
       self.loading(20, "Reading the YNAB data folder ...");
-      self.budgetDataFolder(data.relativeDataFolderName);
-      client.readDir(self.budgetDataPath()).then(function(){
+      self.budgetDataFolder(self.relativeDataFolderName);
+      client.readDir(self.budgetDataPath()).then(function(){        
         self.loading(40, "");
         client.readDir(self.budgetDevicesPath()).then(function(deviceFiles){
           self.loading(60, "Figuring out which device has the latest version ...");
@@ -161,8 +167,19 @@ function BudgetController(settings){
             }else{
               var deviceFilePath = self.deviceFilePath(deviceFile);
               client.loadJson(deviceFilePath).then(function(device){
-                if(device.hasFullKnowledge){
-                  self.device(device);
+                self.devices.push(device);
+                if(self.devices().length > 1) {
+                  var latestDevice = _.max(self.devices(), function(device) {
+                    var versionsKnown = device.knowledge.split(',');
+                      var versionsKnownSum = _.reduce(versionsKnown, function(sum, versionKnown) {
+                        var versionNum = versionKnown.substr(versionKnown.indexOf('-') + 1);
+                          return sum += versionNum;
+                      }, 0);
+                      console.log(versionsKnownSum);
+                      return versionsKnownSum;
+                    });
+                  console.log(latestDevice);
+                  self.device(latestDevice);
                 }
                 callback();
               }).fail(function(){
@@ -172,6 +189,7 @@ function BudgetController(settings){
           }, function(err){
             if(!err) {
               self.loading(90, "Downloading the latest version of the data ...");
+              console.log(self.fullBudgetFile());
               client.loadJson(self.fullBudgetFile()).then(function(budget){
                 self.loading(100);
                 var categories = _.chain(budget.masterCategories).map(function(masterCategory){
@@ -207,7 +225,7 @@ function BudgetController(settings){
         self.errorMessage("Error reading " + self.budgetDataPath())
       })
     }).fail(function(){
-      self.errorMessage("Error loading " + self.budgetMetaPath())
+      self.errorMessage("Error loading " + self.budgetMetaPath)
     })
   }
 }
@@ -247,11 +265,11 @@ function MonthlyCategoryBudget(app, monthlyCategoryBudget) {
 
   self.balance = self.budgeted + self.outflows;
 
-  // todo: adjust for days in month instead of assuming 31
+  var currentDate = new Date();
+  var currentDay = currentDate.getDate()
+  var daysInMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate();
 
-  var currentDay = new Date().getDate();
-
-  self.dailyBudget = self.budgeted / 31;
+  self.dailyBudget = self.budgeted / daysInMonth;
 
   var proratedBudget = self.dailyBudget * currentDay;
 
